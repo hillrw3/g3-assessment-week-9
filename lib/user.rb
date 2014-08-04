7e2dcd394ddbf6ae69315303928576ec55dd8eef
Rob Hill <robert.hill0122@gmail.com>
2014-08-04 10:11:41 -0600

    Fixed bug so users only see their items

--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(username, password)
     find_by(username: username, password: password)
+    # has_many :to_do_items
   end
 
 end
--- spec/features/to_dos_spec.rb
+++ spec/features/to_dos_spec.rb
@@ -61,4 +61,43 @@ feature "ToDos" do
     expect(page).to have_no_content "Become an awesome developer"
     expect(page).to have_content "ToDo Completed"
   end
+
+  scenario "Users only see their ToDo items" do
+    visit "/"
+    click_link "Register"
+
+    fill_in "Username", with: "hunta"
+    fill_in "Password", with: "pazzword"
+
+    click_button "Register"
+
+    fill_in "Username", with: "hunta"
+    fill_in "Password", with: "pazzword"
+
+    click_button "Sign In"
+    fill_in "What do you need to do?", with: "Get a haircut"
+    click_button "Add ToDo"
+
+    expect(page).to have_content "ToDo added"
+
+    click_button "Sign Out"
+
+    click_link "Register"
+
+    fill_in "Username", with: "rob"
+    fill_in "Password", with: "123"
+
+    click_button "Register"
+
+    fill_in "Username", with: "rob"
+    fill_in "Password", with: "123"
+
+    click_button "Sign In"
+
+    fill_in "What do you need to do?", with: "Not see Hunter's ToDo"
+    click_button "Add ToDo"
+
+    expect(page).to have_no_content "Get a haircut"
+    expect(page).to have_content "Not see Hunter's ToDo"
+    end
 end
